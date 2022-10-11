b6a289984ccc7e288e3c9880eba18ef3f93fe11c
Divine T22 <divineson2001@gmail.com>
2022-10-11 12:21:01 +0100

    Update

---
+++ Realhtml/test.js
@@ -0,0 +1,14 @@
+var x="Hello"
+            Hello(x)
+            function Hello(Msg){alert("Welcome to Owerri District Teens Webpage");
+            }
+            function Goodbye(){
+                alert("Welcome")
+            }
+            var nm = prompt("What is your Name");
+            alert(nm); 
+            
+var x="Goodbye"
+            Goodbye(x)
+
+                    
